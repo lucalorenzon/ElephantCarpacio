74ebaf59432a61698e99c43ce5103878491c0f6b
Luca Lorenzon <luca.lorenzon@it.gucci.com>
2023-10-29 11:52:31 +0100

    Luca L | order value with UT tax plus echo state

--- src/use_cases/order_value_calculator/business_logic.rs
+++ src/use_cases/order_value_calculator/business_logic.rs
@@ -13,13 +13,13 @@ impl OrderValueCalculator {
 }
 
 impl ForCalculatingOrderValue for OrderValueCalculator {
-    fn calculate(&self, num_items: i32, price: f32) -> OrderValue {
+    fn calculate(&self, num_items: i32, price: f32, state: &str) -> OrderValue {
         let total_order = num_items as f32 * price;
         let total_discount = total_order * 0f32;
         let total_before_tax = total_order - total_discount;
         let total_tax = total_before_tax * self.tax_rate/100f32;
         let order_value = total_before_tax + total_tax;
-        OrderValue::new(num_items, price, self.tax_rate, order_value)
+        OrderValue::new(num_items, price, state, self.tax_rate, order_value)
     }
 }
 
@@ -34,7 +34,7 @@ mod tests {
         let num_items = 100;
         let price = 10f32;
         let calculator = OrderValueCalculator::new();
-        let result = calculator.calculate(num_items, price);
+        let result = calculator.calculate(num_items, price, "UT");
         assert_eq!(result.num_items, num_items);
         assert_eq!(result.price, price);
     }
@@ -51,9 +51,28 @@ mod tests {
         let expected_order_value = total_before_tax + total_tax;
 
         let calculator = OrderValueCalculator::new();
-        let result = calculator.calculate(num_items, price);
+        let result = calculator.calculate(num_items, price, "UT");
         assert_eq!(result.tax_rate, 6.85);
         assert_eq!(result.order_value, expected_order_value);
+    }
+
+    #[test]
+    fn should_return_order_value_with_0_discount_and_ut_tax_plus_echo_state_ut() {
+        let num_items = 100i32;
+        let price = 10f32;
+        let state = "UT";
+        let tax_rate = 6.85f32;
+        let total_order = num_items as f32 * price;
+        let total_discount = total_order * 0f32;
+        let total_before_tax = total_order - total_discount;
+        let total_tax = total_before_tax * tax_rate/100f32;
+        let expected_order_value = total_before_tax + total_tax;
+
+        let calculator = OrderValueCalculator::new();
+        let result = calculator.calculate(num_items, price, state);
+        assert_eq!(result.tax_rate, 6.85);
+        assert_eq!(result.order_value, expected_order_value);
+        assert_eq!(result.state, state);
     }
 
 }
--- src/use_cases/order_value_calculator/driving_port/for_calculating_order_value.rs
+++ src/use_cases/order_value_calculator/driving_port/for_calculating_order_value.rs
@@ -3,20 +3,22 @@ pub struct OrderValue {
     pub num_items: i32,
     pub price: f32,
     pub tax_rate:f32,
-    pub order_value: f32
+    pub order_value: f32,
+    pub state: String,
 }
 
 impl OrderValue {
-    pub fn new(num_items: i32, price: f32, tax_rate: f32, order_value: f32) -> Self {
+    pub fn new(num_items: i32, price: f32, state:&str, tax_rate: f32, order_value: f32) -> Self {
         OrderValue {
             num_items,
             price,
             tax_rate,
-            order_value
+            order_value,
+            state: state.to_owned(),
         }
     }
 }
 
 pub trait ForCalculatingOrderValue {
-    fn calculate(&self, num_item:i32, price:f32) -> OrderValue;
+    fn calculate(&self, num_item:i32, price:f32, state: &str) -> OrderValue;
 }
